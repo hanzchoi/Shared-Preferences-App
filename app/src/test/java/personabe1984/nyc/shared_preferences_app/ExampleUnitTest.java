34dcf5c8485e8bbe2e96dbb426ed44dc261e8368
personabe1984 <personabe1984@gmail.com>
2016-03-23 19:58:06 -0400

    second commit

---
+++ app/src/test/java/personabe1984/nyc/shared_preferences_app/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package personabe1984.nyc.shared_preferences_app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
