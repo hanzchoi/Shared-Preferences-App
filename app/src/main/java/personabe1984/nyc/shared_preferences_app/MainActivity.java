504a21c8e3a3dd9a22f0d135ac38d88e7cb0b099
personabe1984 <personabe1984@gmail.com>
2016-03-26 12:55:30 -0400

    added material for sharepref

--- app/src/main/java/personabe1984/nyc/shared_preferences_app/MainActivity.java
+++ app/src/main/java/personabe1984/nyc/shared_preferences_app/MainActivity.java
@@ -1,13 +1,26 @@
 package personabe1984.nyc.shared_preferences_app;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String PREFS_FILE = "personabe1984.nyc.shared_preferences_app.preferences";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private EditText mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mEditText = (EditText) findViewById(R.id.editText);
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
     }
 }
