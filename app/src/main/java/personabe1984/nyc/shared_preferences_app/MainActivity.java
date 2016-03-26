a5a3b344016020aba0ad4b1dbc93c1637972e439
personabe1984 <personabe1984@gmail.com>
2016-03-26 13:13:29 -0400

    the edit text will hold the string even after the app closes

--- app/src/main/java/personabe1984/nyc/shared_preferences_app/MainActivity.java
+++ app/src/main/java/personabe1984/nyc/shared_preferences_app/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
 
     private static final String PREFS_FILE = "personabe1984.nyc.shared_preferences_app.preferences";
+    private static final String KEY_EDITTEXT = "key_edittext";
     private SharedPreferences mSharedPreferences;
     private SharedPreferences.Editor mEditor;
     private EditText mEditText;
@@ -22,5 +23,21 @@ public class MainActivity extends AppCompatActivity {
         mEditText = (EditText) findViewById(R.id.editText);
         mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
         mEditor = mSharedPreferences.edit();
+
+        //Gets the string by shared pref and saves it into a string
+        String editTextString = mSharedPreferences.getString(KEY_EDITTEXT, "");
+        mEditText.setText(editTextString);
+    }
+
+    /* Saves the data when android goes onPause */
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        mEditor.putString(KEY_EDITTEXT, mEditText.getText().toString());
+        //saved the changes to share preference
+        mEditor.apply();
     }
 }
+
+//Saving and Retrieving Data with SharedPreferences
